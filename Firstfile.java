b1214c83e2bdd21fc8d4e49d491dee7456eb0dc1
Priya More <priyamore8446@gmail.com>
2026-05-04 17:43:43 +0530

    Input using java

---
+++ Firstfile.java
@@ -0,0 +1,13 @@
+package Pattern_Printing;
+import java.util.Scanner;
+
+public class Firstfile {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+       int sum = a + b;
+        System.out.println("The sum of a & b ="  + sum);
+        sc.close();
+    }
+}
