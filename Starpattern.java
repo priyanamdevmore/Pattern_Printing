4cbdb572b27b4027681bc23be9668043aa5f65e5
Priya More <priyamore8446@gmail.com>
2026-05-04 18:02:00 +0530

    Star pattern printed using java

---
+++ Starpattern.java
@@ -0,0 +1,13 @@
+package Pattern_Printing;
+
+public class Starpattern {
+    public static void main(String[]args){
+        for(int line = 1;line <= 5 ;line++){
+            for(int star = 1; star <= line ; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
