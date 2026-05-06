1406928124ef00269ba28917acfd4bfe7324a20e
Priya More <priyamore8446@gmail.com>
2026-05-06 15:33:56 +0530

    PRINT number pattern instead star

---
+++ NumberPattern.java
@@ -0,0 +1,13 @@
+package Pattern_Printing;
+
+public class NumberPattern {
+    public static void main(String[]args){
+        for(int line = 1;line <=5; line++){
+            for(int num = 1; num<=line;num++){
+                System.out.print(num +" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
