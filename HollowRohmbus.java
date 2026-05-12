0f2936762dab6872b170261bb99bb6d0e9596834
Priya More <priyamore8446@gmail.com>
2026-05-12 19:11:29 +0530

    Try catch block is removed

--- HollowRohmbus.java
+++ HollowRohmbus.java
@@ -3,13 +3,13 @@ package Pattern_Printing;
 public class HollowRohmbus {
     public static void main(String[] args) {
         int size = 5;
-        if (args.length > 0) {
-            try {
-                size = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid size argument, using default size 5.");
-            }
-        }
+        // if (args.length > 0) {
+        //     try {
+        //         size = Integer.parseInt(args[0]);
+        //     } catch (NumberFormatException e) {
+        //         System.out.println("Invalid size argument, using default size 5.");
+        //     }
+        // }
 
         for (int row = 1; row <= size; row++) {
             for (int space = 1; space <= size - row; space++) {
